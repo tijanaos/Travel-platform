4a822e7979d756cc4d12365ec05798cab4e77219
Tijana Ostojic <tijanaostojicns@gmail.com>
2026-03-30 14:50:32 +0200

    feat(stakeholders): add config and main entrypoint

---
+++ Stakeholders/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tijanaos/Stakeholders/config"
+	"github.com/tijanaos/Stakeholders/internal/domain"
+	"github.com/tijanaos/Stakeholders/internal/handler"
+	"github.com/tijanaos/Stakeholders/internal/repository"
+	"github.com/tijanaos/Stakeholders/internal/service"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	cfg := config.Load()
+
+	db, err := gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&domain.User{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	r := gin.Default()
+	userHandler.RegisterRoutes(r)
+
+	log.Printf("Stakeholders service running on port %s", cfg.ServerPort)
+	if err := r.Run(":" + cfg.ServerPort); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
