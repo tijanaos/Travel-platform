0a1bcbca5e2aa948c3677d41f1c2c34d63d4f616
DavidTot <63733844+DavidTot@users.noreply.github.com>
2026-05-17 17:36:59 +0200

    feat: updated frontend to route all requests through gateway

--- Frontend/src/pages/blog/BlogListPage.tsx
+++ Frontend/src/pages/blog/BlogListPage.tsx
@@ -10,7 +10,7 @@ export default function BlogListPage() {
   const { isAuthenticated } = useAuth();
 
   useEffect(() => {
-    blogClient.get('/blogs')
+    blogClient.get('/api/blogs')
       .then(res => setBlogs(res.data))
       .catch(() => {})
       .finally(() => setLoading(false));
